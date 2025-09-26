42b248c0e32ffd8781f075eb1bc3e134b03f8182
Imagury <lenz.felix@gmx.de>
2025-09-26 14:46:07 +0200

    Add and implement cice_convert

---
+++ convert/cice_convert.c
@@ -0,0 +1,99 @@
+// cice_convert.h
+#include	<cice_typedecl.h>
+#include	<cice_math.h>
+
+VOID	CONVERT_StringToInt8	(INT8* buffer, CHAR* in)
+{
+	INT32	length	= 0;
+	while	(in[length] != 0)
+	{
+		length++;
+	}
+
+	INT32	mul	= 0;
+
+	for	(INT32 i = 0; i < length; i++)
+	{
+		if	((in[i] >= '0') && (in[i] <= '9'))
+		{
+			MATH_Power32	(&mul, 10, (length-i-1));
+			*buffer	= *buffer + ((in[i] - '0') * mul);
+		}
+		else
+		{
+			*buffer	= *buffer / 10;
+		}
+	}
+}
+
+VOID	CONVERT_StringToInt16	(INT16* buffer, CHAR* in)
+{
+	INT32	length	= 0;
+	while	(in[length] != 0)
+	{
+		length++;
+	}
+
+	INT32	mul	= 0;
+
+	for	(INT32 i = 0; i < length; i++)
+	{
+		if	((in[i] >= '0') && (in[i] <= '9'))
+		{
+			MATH_Power32	(&mul, 10, (length-i-1));
+			*buffer	= *buffer + ((in[i] - '0') * mul);
+		}
+		else
+		{
+			*buffer	= *buffer / 10;
+		}
+	}
+}
+
+VOID	CONVERT_StringToInt32	(INT32* buffer, CHAR* in)
+{
+	INT32	length	= 0;
+	while	(in[length] != 0)
+	{
+		length++;
+	}
+
+	INT32	mul	= 0;
+
+	for	(INT32 i = 0; i < length; i++)
+	{
+		if	((in[i] >= '0') && (in[i] <= '9'))
+		{
+			MATH_Power32	(&mul, 10, (length-i-1));
+			*buffer	= *buffer + ((in[i] - '0') * mul);
+		}
+		else
+		{
+			*buffer	= *buffer / 10;
+		}
+	}	
+}
+
+VOID	CONVERT_StringToInt64	(INT64* buffer, CHAR* in)
+{
+	INT32	length	= 0;
+	while	(in[length] != 0)
+	{
+		length++;
+	}
+
+	INT32	mul	= 0;
+
+	for	(INT32 i = 0; i < length; i++)
+	{
+		if	((in[i] >= '0') && (in[i] <= '9'))
+		{
+			MATH_Power32	(&mul, 10, (length-i-1));
+			*buffer	= *buffer + ((in[i] - '0') * mul);
+		}
+		else
+		{
+			*buffer	= *buffer / 10;
+		}
+	}
+}
---
+++ convert/cice_convert.h
@@ -0,0 +1,11 @@
+#ifndef	CICE_CONVERT
+#define	CICE_CONVERT
+
+#include	<cice_typedecl.h>
+
+VOID	CONVERT_StringToInt8	(INT8* buffer, CHAR* in);
+VOID	CONVERT_StringToInt16	(INT16* buffer, CHAR* in);
+VOID	CONVERT_StringToInt32	(INT32* buffer, CHAR* in);
+VOID	CONVERT_StringToInt64	(INT64* buffer, CHAR* in);
+
+#endif
